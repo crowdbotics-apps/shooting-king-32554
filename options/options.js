1384e1918ff093c20f19ad40523bfaab81e90f09
Crowdbotics <team@crowdbotics.com>
2021-12-23 09:17:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "shooting_king_32554",
+  url: "https://shooting_king_32554.botics.co",
+  api: "https://shooting_king_32554.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
